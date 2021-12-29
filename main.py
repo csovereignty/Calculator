7e58ec3cd48de9110c561ba1ce152bab730e0afd
csovereignty <christopher.bowenx@gmail.com>
2021-12-29 10:02:13 -0800

    Started tinkering with Tkinter, integration WIP

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from functions import *
+from tkinter import *
 
 calcfunctions.method()
 
 running = True
+
+#Main Window
+mainWindow = Tk()
+#mainWindowTitle = Label(mainWindow, text="Calculatorialism")
+#mainWindowTitle.pack()
+#mainWindow.mainloop()
+mainWindow.geometry('100x100')
+
+btn = Button(mainWindow, text = "Click me!", bd = '5', command=mainWindow.destroy)
+btn.pack(side = 'top')
+
+mainWindow.mainloop()
 
 while(running):
     option = None
