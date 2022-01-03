19961ee57cb3a16bd8b9c6b24cf2a83716d70b4e
csovereignty <christopher.bowenx@gmail.com>
2022-01-03 15:30:55 -0800

    Updated, still more to do (Clean up!)

--- main.py
+++ main.py
@@ -1,55 +1,105 @@
-from functions import *
 from tkinter import *
 
-#calcfunctions.method()
+expression = ""
 
-def menu_select():
-    user_selection = var.get()
-    if user_selection == 1:
-        msg = f'It works, 1 was entered!'
-    else:
-        msg = f'Value other than 1 was entered, but it kind of works!'
-    disp.set(msg)
+def press(num):
+    global expression
+    expression = expression + str(num)
+    equation.set(expression)
+
+def equals():
+    try:
+        global expression
+        total = str(eval(expression))
+        equation.set(total)
+        expresion = ""
+    except:
+        equation.set("Error!")
+        expression = ""
+
+def clear():
+    global expression
+    expression = ""
+    equation.set("")
 
 running = True
 
 #Main Window
 ws = Tk()
 ws.title('Calculatorialism')
-ws.geometry('600x400')
-ws.config(bg='#5671A6')
-
+ws.geometry('270x150')
+ws.config(bg="black")
 var = IntVar()
-disp = StringVar()
+equation = StringVar()
 
-Label(
-    ws,
-    text='Calculatorialism',
-    font=('sans-serif', 20),
-    relief=SOLID,
-    padx=10,
-    pady=10,
-    bg='#F27D16'
-).pack(pady=(10,0))
-
-Entry(
-    ws,
-    textvariable=var,
-    font=('sans-serif', 10)
-).pack(pady=(50,10))
-
-Button(
-    ws,
-    text='Enter selection',
-    font=('sans-serif', 18),
-    command=menu_select
-).pack()
-
-Label(
-    ws,
-    textvariable=disp,
-    bg='#5671A6',
-    font=('sans-serif', 14)
-).pack(pady=(20,0))
+expression_field = Entry(ws, textvariable=equation)
+expression_field.grid(columnspan=4, ipadx=70)
+button1 = Button(ws, text=' 1 ', fg='black', bg='red',
+                    command=lambda: press(1), height=1, width=7)
+button1.grid(row=2, column=0)
+ 
+button2 = Button(ws, text=' 2 ', fg='black', bg='red',
+                    command=lambda: press(2), height=1, width=7)
+button2.grid(row=2, column=1)
+ 
+button3 = Button(ws, text=' 3 ', fg='black', bg='red',
+                    command=lambda: press(3), height=1, width=7)
+button3.grid(row=2, column=2)
+ 
+button4 = Button(ws, text=' 4 ', fg='black', bg='red',
+                    command=lambda: press(4), height=1, width=7)
+button4.grid(row=3, column=0)
+ 
+button5 = Button(ws, text=' 5 ', fg='black', bg='red',
+                    command=lambda: press(5), height=1, width=7)
+button5.grid(row=3, column=1)
+ 
+button6 = Button(ws, text=' 6 ', fg='black', bg='red',
+                    command=lambda: press(6), height=1, width=7)
+button6.grid(row=3, column=2)
+ 
+button7 = Button(ws, text=' 7 ', fg='black', bg='red',
+                    command=lambda: press(7), height=1, width=7)
+button7.grid(row=4, column=0)
+ 
+button8 = Button(ws, text=' 8 ', fg='black', bg='red',
+                    command=lambda: press(8), height=1, width=7)
+button8.grid(row=4, column=1)
+ 
+button9 = Button(ws, text=' 9 ', fg='black', bg='red',
+                    command=lambda: press(9), height=1, width=7)
+button9.grid(row=4, column=2)
+ 
+button0 = Button(ws, text=' 0 ', fg='black', bg='red',
+                    command=lambda: press(0), height=1, width=7)
+button0.grid(row=5, column=0)
+ 
+plus = Button(ws, text=' + ', fg='black', bg='red',
+                command=lambda: press("+"), height=1, width=7)
+plus.grid(row=2, column=3)
+ 
+minus = Button(ws, text=' - ', fg='black', bg='red',
+                command=lambda: press("-"), height=1, width=7)
+minus.grid(row=3, column=3)
+ 
+multiply = Button(ws, text=' * ', fg='black', bg='red',
+                    command=lambda: press("*"), height=1, width=7)
+multiply.grid(row=4, column=3)
+ 
+divide = Button(ws, text=' / ', fg='black', bg='red',
+                    command=lambda: press("/"), height=1, width=7)
+divide.grid(row=5, column=3)
+ 
+equal = Button(ws, text=' = ', fg='black', bg='red',
+                command=equals, height=1, width=7)
+equal.grid(row=5, column=2)
+ 
+clear = Button(ws, text='Clear', fg='black', bg='red',
+                command=clear, height=1, width=7)
+clear.grid(row=5, column='1')
+ 
+Decimal= Button(ws, text='.', fg='black', bg='red',
+                    command=lambda: press('.'), height=1, width=7)
+Decimal.grid(row=6, column=0)
 
 ws.mainloop()
